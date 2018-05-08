619f3e14e2fbb375a52c7330bc7f7deb9cd11c9c
RenatoSpll <renatomacedo500@gmail.com>
2018-05-08 13:31:18 -0300

    Enviar para Othon

--- app/src/main/java/com/renatomacedo/chamada/MainActivity.kt
+++ app/src/main/java/com/renatomacedo/chamada/MainActivity.kt
@@ -9,23 +9,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //val db = DatabaseHelper(this)
+        val db = DatabaseHelper(this)
         //db.addProfessor(1,"Othon Batista","deadpoolothon","Computação para Dispositvos Móveis")
         //db.addProfessor(4,"Clevenson Mineiro","saraudaonça123","Matemática")
         //db.delete(1,"Professor")
         var aluno1: Aluno = Aluno(12,"Renato","MR01")
-        var aluno2: Aluno = Aluno(20,"Jonas","MR02")
-        var aluno3: Aluno = Aluno(7,"Rigeraldo","MR05")
+        //var aluno2: Aluno = Aluno(20,"Jonas","MR02")
+        //var aluno3: Aluno = Aluno(7,"Rigeraldo","MR05")
 
 
-        //db.addAluno(aluno1)
+        db.addAluno(aluno1)
         //db.addAluno(aluno2)
         //db.addAluno(aluno3)
         //db.delete(7,"Aluno")
--- app/src/main/java/com/renatomacedo/chamada/NewActivity2.kt
+++ app/src/main/java/com/renatomacedo/chamada/NewActivity2.kt
@@ -9,16 +9,16 @@ class NewActivity2: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new2)
         var db = DatabaseHelper(this)
-        var aluno1: Aluno = Aluno(12,"Renato","MR01")
-        db.addAluno(aluno1)
+        //var aluno1: Aluno = Aluno(12,"Renato","MR01")
+        //db.addAluno(aluno1)
 
 
             var infodobanco =db.read()
-            //txtAluno.text = infodobanco.get(0).nome
-
+            txtAluno.text = infodobanco.get(0).nome
+/*
         for(i in 0..infodobanco.size-1){
-                txtAluno.append(infodobanco.get(i).matricula.toString()+" "+infodobanco.get(i).nome+" "+infodobanco.get(i).turma+"\n")
+                txtAluno.setText(infodobanco.get(i).matricula.toString()+" "+infodobanco.get(i).nome+" "+infodobanco.get(i).turma+"\n")
             }
-
+*/
     }
 }
